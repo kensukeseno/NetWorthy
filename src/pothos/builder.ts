43fc8b9fa7297cc48a7822c8e7475e7138a171cd
ken <kensukemoneymoney@gmail.com>
2025-06-13 15:57:16 -0600

    Jun-13-2025 integrate-pothos

---
+++ src/pothos/builder.ts
@@ -0,0 +1,16 @@
+// /pothos/builder.ts
+import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes;
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+  },
+});
